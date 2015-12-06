2e31fa8e5b9ddd78cc506b6aeb47b04614485dd1
Sean Tracey <seanmtracey@gmail.com>
2015-12-06 15:05:06 +0000

    sonic coder actually points to sonic coder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
 	server : require("./lib/sonic-server.js"),
 	socket : require("./lib/sonic-socket.js"),
-	coder : require("./lib/sonic-server.js")
+	coder : require("./lib/sonic-coder.js")
 }
